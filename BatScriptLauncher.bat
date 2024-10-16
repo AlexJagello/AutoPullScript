e8525fbe7b920476bf7b6b8efe1cbd95a146fdd0
Alex <54600063+AlexJagello@users.noreply.github.com>
2024-10-16 16:12:02 +0300

    Update BatScriptLauncher.bat

--- BatScriptLauncher.bat
+++ BatScriptLauncher.bat
@@ -1,5 +1,5 @@
 @echo off]
-set pathToScript=C:/Users/sasha/Desktop/Radar
+set pathToScript=C:/Users/sasha/Desktop/MyRepositories
 cd %pathToScript%
 echo %pathToScript%
 start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login -i -- AutoGitPullScript.bash
